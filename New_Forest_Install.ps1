4dba589f9808c7f73e8eeab58ee92eb107a6fed4
YoucefDahak <116385867+YoucefDahak@users.noreply.github.com>
2022-10-21 19:05:25 -0400

    Update New_Forest_Install.ps1

--- New_Forest_Install.ps1
+++ New_Forest_Install.ps1
@@ -3,15 +3,4 @@
 #
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
-Install-ADDSForest `z
--CreateDnsDelegation "false" `
--DatabasePath "C:\Windows\NTDS" `
--DomainMode "WinThreshold" `
--DomainName "lab.home.ca" `
--DomainNetbiosName "LAB" `
--ForestMode "WinThreshold" `
--InstallDns "true" `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion "false" `
--SysvolPath "C:\Windows\SYSVOL" `
--Force "true"
+Install-ADDSForest -CreateDnsDelegation "false" -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "lab.home.ca" -DomainNetbiosName "LAB" -ForestMode "WinThreshold" -InstallDns "true" -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion "false" -SysvolPath "C:\Windows\SYSVOL" -Force "true"
