f77112339f65cae1ad865b1c4438cd6c48a69cad
YoucefDahak <116385867+YoucefDahak@users.noreply.github.com>
2022-10-25 18:49:20 -0400

    Update New_Forest_Install.ps1

--- New_Forest_Install.ps1
+++ New_Forest_Install.ps1
@@ -1,5 +1,5 @@
 #
 # Windows PowerShell script for AD DS Deployment
 #
-New-Item -Path "c:\\install3333" -ItemType directory"'
-Copy-Item -Path \\dc01.lab.home.ca\\share\\SSMS-Setup-ENU.exe -Destination c:\\install
+New-Item -Path "c:\\install3333" -ItemType directory"
+Copy-Item -Path "\\dc01.lab.home.ca\\share\\SSMS-Setup-ENU.exe" -Destination "c:\\install"
